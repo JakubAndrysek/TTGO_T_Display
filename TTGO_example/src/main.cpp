da93257a266fd8404eecab7d02145c06d24bf753
JarekParal <paral.jarek@gmail.com>
2019-11-23 14:09:30 +0100

    Fix configuration of the project for PlatformIO
    - now the display work correctly
    - set definitions for project to `platformio.ino`
    - copy the definitions from `Setup25_TTGO_T_Display.h`
    - inspiration: https://github.com/Bodmer/TFT_eSPI/issues/319#issuecomment-494800308

--- TTGO_example/src/main.cpp
+++ TTGO_example/src/main.cpp
@@ -16,13 +16,6 @@
 #define TFT_SLPIN   0x10
 #endif
 
-#define TFT_MOSI            19
-#define TFT_SCLK            18
-#define TFT_CS              5
-#define TFT_DC              16
-#define TFT_RST             23
-
-#define TFT_BL          4  // Display backlight control pin
 #define ADC_EN          14
 #define ADC_PIN         34
 #define BUTTON_1        35
